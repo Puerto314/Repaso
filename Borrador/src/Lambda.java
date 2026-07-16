8f56ce63b4f75afce9fea89dff543bda9cd5c215
Esteban <estebanpuertorojas235777@gmail.com>
2026-07-16 18:44:40 -0500

    Lambda

---
+++ Borrador/src/Lambda.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+interface EsPar {
+
+	boolean esPar(int n);
+
+}
+
+interface Sumar {
+
+	int sumar(int a, int b);
+
+}
+
+public class Lambda {
+
+	public static void main(String[] args) throws IOException {
+
+		EsPar p = (n) -> n % 2 == 0;
+		System.out.println(p.esPar(3));
+
+		ArrayList<String> lista = new ArrayList<>();
+		lista.add("Esteban");
+		lista.add("Puerto");
+		lista.add("Rojas");
+		lista.forEach((n) -> System.out.println(n));
+		lista.forEach(System.out::println);
+
+		Sumar s = (a, b) -> a + b;
+		System.out.println(s.sumar(5, 4));
+
+	}
+
+}
