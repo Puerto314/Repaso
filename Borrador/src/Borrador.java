9993fd445f5ce6ac5bb52bb3aa64a84cd03c815e
Esteban <estebanpuertorojas235777@gmail.com>
2026-07-15 21:15:48 -0500

    Prueba de commit

--- Borrador/src/Borrador.java
+++ Borrador/src/Borrador.java
@@ -1,7 +1,12 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Borrador {
 
 	public static void main(String[] args) {
+
+		System.out.println("Sí funciona");
 
 	}
 
